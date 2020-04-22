ad230dd5a83ae1c31b93b8c866eb0eb5e83e435d
xiaoyuntongxue <huxiaoyun1997@163.com>
2020-04-22 15:18:27 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import './assets/css/global.css'
 // 配置axios
 import axios from 'axios'
 Vue.prototype.$http = axios;
+
 // 配置访问的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
